14e2a43325ce8c1c752c2efc00d63345fbe3703d
SR <865459737@qq.com>
2019-05-25 17:01:28 +0800

    change code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+   
+
+    let price=0;
+    if(inputs.distance<2){
+        price=6;
+    }
+    else if(inputs.distance<8){
+        price=6+(inputs.distance-2)*0.8;
+    }
+    else{
+        price=6+(8-2)*0.8+(inputs.distance-8)*0.8*(1+0.5);
+    }
+    price+=inputs.parkTime*0.25;
+    return Math.round(price);
 };
